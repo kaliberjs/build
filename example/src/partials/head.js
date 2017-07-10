b521b4fb0ba4bd87517a9cb1aefdd0f327594713
klaasman <klaaslandsman@gmail.com>
2017-07-10 16:59:53 +0200

    Show in example how to require minified react for production

--- example/src/partials/head.js
+++ example/src/partials/head.js
@@ -2,15 +2,17 @@ import styles from './head.css'
 import x from 'test/test.entry'
 import y from 'test/test.css'
 
+const isProduction = process.env.NODE_ENV === 'production'
+
 export default function(title) {
-	return (
-		<head>
-			<title>{title}</title>
+  return (
+    <head>
+      <title>{title}</title>
       { /* we might be able to inject these (or supply them as props) */}
       <link href='/index.css' rel='stylesheet' type='text/css' />
       <link href='https://cdnjs.cloudflare.com/ajax/libs/normalize/7.0.0/normalize.css' rel='stylesheet' type='text/css' />
-      <script src='https://unpkg.com/react@15.6.1/dist/react.js' />
-      <script src='https://unpkg.com/react-dom@15.6.1/dist/react-dom.js' />
-		</head>
-	)
+      <script defer src={`https://unpkg.com/react@15.6.1/dist/react${isProduction ? '.min' : ''}.js`} />
+      <script defer src={`https://unpkg.com/react-dom@15.6.1/dist/react-dom${isProduction ? '.min' : ''}.js`} />
+    </head>
+  )
 }
