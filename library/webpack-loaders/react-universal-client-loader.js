31ff9235d65d38b18b709b5d5eea6f1ca391a9a8
Klaas Landsman <klaasman@users.noreply.github.com>
2017-12-01 10:01:23 +0100

    `reactDOM.render` is deprecated in React v16, use `hydrate` instead

--- library/webpack-loaders/react-universal-client-loader.js
+++ library/webpack-loaders/react-universal-client-loader.js
@@ -11,15 +11,15 @@ function ReactUniversalClientLoader(source, map) {
 
 function createClientCode({ importPath, id }) {
   return `|import Component from './${importPath}'
-          |import { render } from 'react-dom'
+          |import { hydrate } from 'react-dom'
           |
           |const element = document.getElementById('${id}')
           |const props = JSON.parse(element.dataset.props)
-          |render(<Component {...props} />, element)
+          |hydrate(<Component {...props} />, element)
           |
           |if (module.hot) {
           |  require('@kaliber/build/lib/hot-module-replacement-client')
-          |  module.hot.accept('./${importPath}', () => { render(<Component {...props} />, element) })
+          |  module.hot.accept('./${importPath}', () => { hydrate(<Component {...props} />, element) })
           |}
           |`.split(/^[ \t]*\|/m).join('')
 }
