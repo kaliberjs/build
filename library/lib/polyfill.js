a2700bf84ca45a4b2ebf4b636bc9556bc57d7385
Klaas Landsman <klaaslandsman@gmail.com>
2018-10-23 11:25:51 +0200

    disable polyifll.io real user monitoring

--- library/lib/polyfill.js
+++ library/lib/polyfill.js
@@ -2,6 +2,6 @@ const isProduction = process.env.NODE_ENV === 'production'
 
 export default function polyfill(features = []) {
   return isProduction
-    ? <script defer src={`https://cdn.polyfill.io/v2/polyfill.min.js?features=${features.join(',')}`} crossorigin="anonymous" />
-    : <script defer src={`https://cdn.polyfill.io/v2/polyfill.js?features=${features.join(',')}`} crossorigin="anonymous" />
+    ? <script defer src={`https://cdn.polyfill.io/v2/polyfill.min.js?rum=0&features=${features.join(',')}`} crossorigin="anonymous" />
+    : <script defer src={`https://cdn.polyfill.io/v2/polyfill.js?rum=0&features=${features.join(',')}`} crossorigin="anonymous" />
 }
