4f528285de268ae0b17dce071e77937208284242
Erik Westra <ewestra@kaliber.net>
2019-02-08 13:20:10 +0100

    removed postcss plugin composition

--- library/webpack-loaders/css-loader.js
+++ library/webpack-loaders/css-loader.js
@@ -98,7 +98,7 @@ module.exports = function CssLoader(source, map) {
   function resolve(context, request) {
     return new Promise((resolve, reject) => {
       self.resolve(context, request, (err, result) => { err ? reject(err) : resolve(result) })
-    }).catch(e => { callback(e) }) // this should not be required, by it seems postcss-plugin-composition does not pass along 'warnings' (more commonly known as 'errors')
+    }).catch(e => { callback(e) }) // this should not be required, by it seems postcss-plugin-composition does not pass along 'warnings' (more commonly known as 'errors') - this should be tested again
   }
 
   function loadModule(url) {
