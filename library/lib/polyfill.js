b086b8d7e64eaa258987790f6b7a3df990083530
Erik Westra <ewestra@kaliber.net>
2019-02-19 11:14:38 +0100

    updated polyfill service to v3

--- library/lib/polyfill.js
+++ library/lib/polyfill.js
@@ -2,7 +2,7 @@ const isProduction = process.env.NODE_ENV === 'production'
 
 export default function polyfill(features = []) {
   const src = isProduction
-    ? `https://cdn.polyfill.io/v2/polyfill.min.js?rum=0&unknown=polyfill&features=${features.join(',')}`
-    : `https://cdn.polyfill.io/v2/polyfill.js?rum=0&unknown=polyfill&features=${features.join(',')}`
+    ? `https://cdn.polyfill.io/v3/polyfill.min.js?rum=0&unknown=polyfill&features=${features.join(',')}`
+    : `https://cdn.polyfill.io/v3/polyfill.js?rum=0&unknown=polyfill&features=${features.join(',')}`
   return <script defer src={src} crossorigin="anonymous" />
 }
