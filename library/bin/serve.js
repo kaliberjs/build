4bc61c3fc16cfb5909a92f94b3114e42c6d9c8fe
larsvankleef <larsvankleef@outlook.com>
2019-08-29 14:46:08 +0200

    Working version with new bin system

--- library/bin/serve.js
+++ library/bin/serve.js
@@ -1,6 +1,3 @@
-#!/bin/sh
-":" //# comment; exec /usr/bin/env node --icu-data-dir="$( node -e 'process.stdout.write(require(`path`).dirname(require.resolve(`full-icu`)))' )" "$0" "$@"
-
 const checkInternationalization = require('../lib/checkInternationalization')
 checkInternationalization()
 
