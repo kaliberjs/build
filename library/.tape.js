957509f73a3864b453fe11a40c09eb545a115210
EECOLOR <eecolor@qirx.org>
2019-09-28 01:25:36 +0200

    cleanup, added rules and separated rules

--- library/.tape.js
+++ library/.tape.js
@@ -1,31 +1,68 @@
 const { messages } = require('./stylelint-plugins/kaliber')
 
+function message(key) {
+  const x = messages[key]
+  return x || `programming error, message with key '${key}' not found`
+}
+
 module.exports = {
-  'kaliber/stylelint': [
+  'kaliber/valid-stacking-context-in-root': [
     {
-      source: '.test { z-index: 0; }',
-      warnings: [messages['root - z-index without position relative']]
+      source: '.bad { z-index: 0; }',
+      warnings: [message('root - z-index without position relative')]
     },
     {
-      source: '.test { position: relative; z-index: 1; }',
-      warnings: [messages['root - z-index not 0']]
+      source: '.bad { position: relative; z-index: 1; }',
+      warnings: [message('root - z-index not 0')]
     },
-    { source: '.test { position:relative; z-index: 0; }', warnings: 0 },
+    { source: '.good { position: relative; z-index: 0; }', warnings: 0 },
+  ],
+  'kaliber/require-stacking-context-in-parent': [
     {
-      source: '.test1 { & > test2 { z-index: 0; } }',
-      warnings: [messages['nested - missing stacking context in parent']]
-    },
-    {
-      source: '.test1 { position: relative; & > test2 { z-index: 0; } }',
-      warnings: [messages['nested - missing stacking context in parent']]
+      source: '.bad { & > .test { z-index: 0; } }',
+      warnings: [message('nested - missing stacking context in parent')]
     },
     {
-      source: '.test1 { z-index: 0; & > test2 { z-index: 0; } }',
+      source: '.bad { position: relative; & > .test { z-index: 0; } }',
+      warnings: [message('nested - missing stacking context in parent')]
+    },
+    {
+      source: '.bad { z-index: 0; & > .test { z-index: 0; } }',
+      warnings: [message('nested - missing stacking context in parent')]
+    },
+    { source: '.good { position: relative; z-index: 0; & > .test { z-index: 1; } }', warnings: 0 },
+  ],
+  'kaliber/no-layout-related-props-in-root': [
+    {
+      source: '.bad { width: 100%; height: 100%; position: absolute; }',
       warnings: [
-        messages['root - z-index without position relative'],
-        messages['nested - missing stacking context in parent']
+        message('root - no layout related props')('width'),
+        message('root - no layout related props')('height'),
+        message('root - no layout related props')('position: absolute'),
       ]
     },
-    { source: '.test { position:relative; z-index: 0; & > test2 { z-index: 0; } }', warnings: 0 },
+    { source: '.good { & > .test { width: 100%; height: 100%; position: absolute; } }', warnings: 0 }
+  ],
+  'kaliber/no-double-nesting': [
+    {
+      source: '.bad { & > .test1 { & > .test2 { } } }',
+      warnings: [message('nested - no double nesting')]
+    },
+    { source: '.good { & > .test { } }', warnings: 0 }
+  ],
+  'kaliber/absolute-has-relative-parent': [
+    {
+      source: '.bad { & > .test { position: absolute; } }',
+      warnings: [message('nested - absolute has relative parent')]
+    },
+    { source: '.good { position: relative; & > .test { position: absolute; } }'}
+  ],
+  'kaliber/only-layout-related-props-in-nested': [
+    {
+      source: '.bad { & > .test { padding: 100px; } }',
+      warnings: [message('nested - only layout related props in nested')('padding')]
+    },
+    { source: '.good { & > .test { width: 100%; } }', warnings: 0 },
+    { source: '.good { padding: 100px; }', warnings: 0 }
   ]
 }
--- library/stylelint-plugins/kaliber.js
+++ library/stylelint-plugins/kaliber.js
@@ -1,67 +1,265 @@
 const stylelint = require('stylelint')
 
-const ruleName = 'kaliber/stylelint'
-const messages =  stylelint.utils.ruleMessages(ruleName, {
-  'root - z-index without position relative': 'missing position: relative;\n\n`z-index` can only be used at the root level when creating a non invasive stacking context - add `position: relative;` or set `z-index` from parent',
-  'root - z-index not 0': 'not 0\n\n`z-index` can only be used at the root level when creating a non invasive stacking context - set to 0 or set `z-index` from parent',
-  'nested - missing stacking context in parent': 'missing stacking context (position: relative; z-index: 0;)\n\n`z-index` can only be used when the parent creates a new stacking context - add `position: relative;` and `z-index: 0` to parent',
-  // doubleNesting: 'nesting too deep',
-  // onlyTopLevel: prop => `'${prop}' should only appear in top level rules`,
-  // invalidZIndexContext: 'can only use z-index if parent creates a neutral stacking context',
-  // invalidZIndexAtRoot: 'z-index on root level can only be used to create a stacking context, set value from parent or set it to 0',
-  // invalidPositionAbsoluteValueAtRoot: 'position can not be absolute at root, set from parent',
-  // onlyZIndexForStackingContextAtRoot: 'z-index is only valid if you use it to create a stacking context (add position: relative)'
-})
+const layoutRelatedProps = ['width', 'height', ['position', 'absolute']]
+const layoutRelatedPropsWithValues = extractPropsWithValues(layoutRelatedProps)
 
-module.exports = stylelint.createPlugin(ruleName, (primaryOption, secondaryOptionObject) => {
-  return function(root, result) {
-    const validOptions = stylelint.utils.validateOptions(result, ruleName, { actual: primaryOption, possible: [true] })
-    if (!validOptions) { return }
+const rules = /** @type {any[] & { messages: { [key: string]: any } }} */ ([
+  requireStackingContextInParent(),
+  validStackingContextInRoot(),
+  noLayoutRelatedPropsInRoot(),
+  noDoubleNesting(),
+  absoluteHasRelativeParent(),
+  onlyLayoutRelatedPropsInNested(),
+])
+rules.messages = rules.reduce((result, x) => ({ ...result, ...x.rawMessages }), {})
+module.exports = rules
 
-    root.walkRules(rule => {
-      // console.log(rule.selector)
-      const parent = getParentRule(rule)
-
-      if (parent) {
-        // if (getParent(parent)) report(rule, messages.doubleNesting)
-        const parentDecls = getDecls(parent)
-
-        rule.each(x => {
-          if (x.type === 'decl') {
-            // if (x.prop === 'padding') report(x, messages.onlyTopLevel('padding'))
-            if (x.prop === 'z-index' && (parentDecls['z-index'] !== '0' || parentDecls['position'] !== 'relative')) report(x, messages['nested - missing stacking context in parent'])
-          }
-        })
-      } else {
-        const decls = getDecls(rule)
-        rule.each(x => {
-          if (x.type === 'decl') {
-            if (x.prop === 'z-index') {
-              if (decls['position'] !== 'relative') report(x, messages['root - z-index without position relative'])
-              if (x.value !== '0') report(x, messages['root - z-index not 0'])
-            }
-            // if (x.prop === 'position' && x.value === 'absolute') report(x, messages.invalidPositionAbsoluteValueAtRoot)
-          }
-        })
-      }
-    })
-
-    function report(node, message) {
-      stylelint.utils.report({ message, node, result, ruleName })
-    }
-
+function absoluteHasRelativeParent() {
+  const messages = {
+    'nested - absolute has relative parent':
+      'missing `position: relative;` in parent\n\n' +
+      '`position: absolute` is only allowed when the containing root rule is set to `position: relative` -' +
+      'add `position: relative;` to the containing root rule'
   }
-})
+  return createPlugin({
+    ruleName: 'kaliber/absolute-has-relative-parent',
+    messages,
+    plugin: ({ root, report }) => {
+      withNestedRules(root, (rule, parent) => {
+        const decl = findDecl(rule, 'position')
+        if (!decl || decl.value !== 'absolute') return
 
-module.exports.ruleName = ruleName
-module.exports.messages = messages
+        const parentDecl = findDecl(parent, 'position')
+        if (!parentDecl || parentDecl.value !== 'relative')
+          report(decl, messages['nested - absolute has relative parent'])
+      })
+    }
+  })
+}
+
+function noDoubleNesting() {
+  const messages = {
+    'nested - no double nesting':
+      'no double nesting\n\n' +
+      'nesting is only allowed one level - ' +
+      'create a root rule and move the nested block there'
+  }
+  return createPlugin({
+    ruleName: 'kaliber/no-double-nesting',
+    messages,
+    plugin: ({ root, report }) => {
+      withNestedRules(root, (rule, parent) => {
+        if (getParentRule(parent)) report(rule, messages['nested - no double nesting'])
+      })
+    }
+  })
+}
+
+function requireStackingContextInParent() {
+  const messages = {
+    'nested - missing stacking context in parent':
+      'missing stacking context (`position: relative; z-index: 0;`)\n\n' +
+      '`z-index` can only be used when the containing root rule creates a new stacking context - ' +
+      'add `position: relative;` and `z-index: 0` to the containing root rule',
+  }
+  return createPlugin({
+    ruleName: 'kaliber/require-stacking-context-in-parent',
+    messages,
+    plugin: ({ root, report }) => {
+      withNestedRules(root, (rule, parent) => {
+
+        const decl = findDecl(rule, 'z-index')
+        if (!decl) return
+
+        if (missingProps(parent, { 'z-index': '0', 'position': 'relative' }))
+        report(decl, messages['nested - missing stacking context in parent'])
+      })
+    },
+  })
+}
+
+function validStackingContextInRoot() {
+  const messages = {
+    'root - z-index without position relative':
+      'missing `position: relative;`\n\n' +
+      '`z-index` can only be used at the root level to create a non invasive stacking context - ' +
+      'add `position: relative;` or set the `z-index` with a nested selector in another root rule',
+    'root - z-index not 0':
+      'not 0\n\n' +
+      '`z-index` can only be used at the root level when creating a non invasive stacking context - ' +
+      'set to 0 or set the `z-index` with a nested selector in another root rule',
+  }
+  return createPlugin({
+    ruleName: 'kaliber/valid-stacking-context-in-root',
+    messages,
+    plugin: ({ root, report }) => {
+      withRootRules(root, rule => {
+
+        const decl = findDecl(rule, 'z-index')
+        if (!decl) return
+
+        if (missingProps(rule, { 'position': 'relative' }))
+          report(decl, messages['root - z-index without position relative'])
+
+        if (decl.value !== '0')
+          report(decl, messages['root - z-index not 0'])
+      })
+    },
+  })
+}
+
+function noLayoutRelatedPropsInRoot() {
+  const messages = {
+    'root - no layout related props': prop =>
+      `illegal layout related prop\n\n` +
+      `\`${prop}\` can only be used by root rules in nested selectors - ` +
+      `move to a nested selector in a another root rule`
+  }
+  return createPlugin({
+    ruleName: 'kaliber/no-layout-related-props-in-root',
+    messages,
+    plugin: ({ root, report }) => {
+      withRootRules(root, rule => {
+
+        const decls = findDecls(rule, layoutRelatedProps)
+        decls.forEach(decl => {
+          const { prop } = decl
+          const value = layoutRelatedPropsWithValues[prop]
+          report(decl, messages['root - no layout related props'](prop + (value ? `: ${value}` : '')))
+        })
+      })
+    }
+  })
+}
+
+function onlyLayoutRelatedPropsInNested() {
+  const messages = {
+    'nested - only layout related props in nested':  prop =>
+      `illegal non-layout related prop\n\n` +
+      `\`${prop}\` can only be used by root rules - ` +
+      `move to another root rule`
+  }
+  return createPlugin({
+    ruleName: 'kaliber/only-layout-related-props-in-nested',
+    messages,
+    plugin: ({ root, report }) => {
+      withNestedRules(root, (rule, parent) => {
+        const decls = findDecls(rule, layoutRelatedProps, { onlyInvalidTargets: true })
+        decls.forEach(decl => {
+          report(decl, messages['nested - only layout related props in nested'](decl.prop))
+        })
+      })
+    }
+  })
+}
+
+function extractPropsWithValues(props) {
+  return props.reduce(
+    (result, x) => {
+      if (Array.isArray(x)) {
+        const [prop, value] = x
+        return { ...result, [prop]: value }
+      } else return result
+    },
+    {}
+  )
+}
+
+function withNestedRules(root, f) {
+  root.walkRules(rule => {
+    const parent = getParentRule(rule)
+    if (!parent) return
+    f(rule, parent)
+  })
+}
+
+function withRootRules(root, f) {
+  root.walkRules(rule => {
+    const parent = getParentRule(rule)
+    if (parent) return
+    f(rule)
+  })
+}
+
+function findDecls(rule, targets, { onlyInvalidTargets = false } = {}) {
+  let result = []
+  const normalizedTargets = targets.reduce(
+    (result, x) => {
+      const [name, value] = Array.isArray(x) ? x : [x, '']
+      return { ...result, [name]: value }
+    },
+    {}
+  )
+
+  rule.each(node => {
+    if (
+      node.type !== 'decl' ||
+      onlyInvalidTargets && !invalidTarget(normalizedTargets, node) ||
+      !onlyInvalidTargets && invalidTarget(normalizedTargets, node)
+    ) return
+
+    result.push(node)
+    const continueIteration = !onlyInvalidTargets && result.length !== targets.length
+    return continueIteration
+  })
+
+  return result
+}
+
+function invalidTarget(targets, { prop, value }) {
+  const hasProp = targets.hasOwnProperty(prop)
+  const targetValue = targets[value]
+  return !hasProp || (targetValue && targetValue !== value)
+}
+
+function findDecl(rule, name) {
+  const [result] = findDecls(rule, [name])
+  return result
+}
+
+function missingProps(target, requiredProps) {
+  const actualProps = getProps(target)
+  return Object.entries(requiredProps).reduce(
+    (invalid, [prop, value]) => invalid || actualProps[prop] !== value,
+    false
+  )
+}
+
+function createPlugin({ ruleName, messages, plugin }) {
+  const stylelintPlugin = stylelint.createPlugin(ruleName, pluginWrapper)
+
+  return {
+    ...stylelintPlugin,
+    rawMessages: messages,
+    messages: stylelint.utils.ruleMessages(ruleName, messages),
+    ruleName
+  }
+
+  function pluginWrapper(primaryOption, secondaryOptionObject) {
+    return (root, result) => {
+      const check = { actual: primaryOption, possible: [true] }
+      if (!stylelint.utils.validateOptions(result, ruleName, check)) return
+
+      plugin({ root, report })
+
+      function report(node, message) { stylelint.utils.report({ message, node, result, ruleName }) }
+    }
+  }
+}
+
+  // onlyTopLevel: prop => `'${prop}' should only appear in top level rules`,
+  // invalidPositionAbsoluteValueAtRoot: 'position can not be absolute at root, set from parent',
+
+  // if (getParent(parent)) report(rule, messages.doubleNesting)
+  // if (x.prop === 'padding') report(x, messages.onlyTopLevel('padding'))
+  // if (x.prop === 'position' && x.value === 'absolute') report(x, messages.invalidPositionAbsoluteValueAtRoot)
 
 function getParentRule({ parent }) {
   return parent.type !== 'root' &&
         (parent.type === 'rule' ? parent : getParentRule(parent))
 }
 
-function getDecls(node) {
+function getProps(node) {
   const result = {}
   node.each(x => {
     if (x.type === 'decl') result[x.prop] = x.value
