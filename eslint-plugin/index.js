24398cadb7221aee7e412e839e91586371791e2e
EECOLOR <eecolor@qirx.org>
2019-10-06 23:16:33 +0200

    consistently use early returns

--- eslint-plugin/index.js
+++ eslint-plugin/index.js
@@ -24,7 +24,9 @@ module.exports = {
 
             if (!isRootJSXElement(jsxElement)) return
             const expected = `component${getFunctionName(context)}`
-            if (node.property.name !== expected) context.report({
+            if (node.property.name === expected) return
+
+            context.report({
               message: messages['invalid className'](expected),
               node: node.property,
             })
@@ -38,8 +40,9 @@ module.exports = {
           [`ReturnStatement JSXAttribute[name.name = 'className'] MemberExpression[object.name = 'styles']`](node) {
             const jsxElement = getParentJSXElement(node)
 
-            if (isRootJSXElement(jsxElement)) return
-            if (node.property.name.startsWith('component')) context.report({
+            if (isRootJSXElement(jsxElement) || !node.property.name.startsWith('component')) return
+
+            context.report({
               message: messages['no component className'],
               node: node.property,
             })
