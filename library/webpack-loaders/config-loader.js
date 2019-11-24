650636c479511599f153cbdae17c7c2f77a9ac53
EECOLOR <eecolor@qirx.org>
2019-11-24 22:51:17 +0100

    allow any config phase

--- library/webpack-loaders/config-loader.js
+++ library/webpack-loaders/config-loader.js
@@ -1,18 +1,9 @@
-/*
-  Instead of loading @kaliber/config we execute it, remove the `kaliber` key and stringify the result
-*/
-
-const supportedConfigPhases = ['dev', 'tst', 'acc', 'prd']
-
 module.exports = ConfigLoader
 
+/** @type {import('webpack').loader.Loader} */
 function ConfigLoader() {}
 
 ConfigLoader.pitch = function ConfigLoaderPitch() {
-
-  if (process.env.CONFIG_ENV && !supportedConfigPhases.includes(process.env.CONFIG_ENV)) {
-    throw new Error(`Unsupported CONFIG_ENV \`${process.env.CONFIG_ENV}\`, expected one of ${supportedConfigPhases}`)
-  }
 
   const result = require('@kaliber/config')
 
