43162e0084e56e9d6281a4234ddf942af29cb61f
Joost Kiens <me@joostkiens.com>
2020-03-23 16:23:36 +0100

    add rollbar config example

--- library/template/config/default.js
+++ library/template/config/default.js
@@ -3,4 +3,7 @@ module.exports = {
     Warning: do not use this to set values that differ in each environment,
     only use this for configuration that is the same across all config environments
   */
+  rollbar: {
+    accessToken: 'get an access token at rollbar.com',
+  }
 }
