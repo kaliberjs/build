2dace2820eba2fcc911cfed441d7d613dab9dfd1
larsvankleef <larsvankleef@outlook.com>
2021-04-13 15:03:15 +0200

    Fixed loader to return the path and updated the example

--- example/src/test/Test2.js
+++ example/src/test/Test2.js
@@ -1,4 +1,3 @@
-import { useWebWorker } from './test-worker?webworker'
 // eslint-disable-next-line @kaliber/no-default-export
 export default class Test2 extends Component {
 
@@ -20,14 +19,17 @@ export default class Test2 extends Component {
 
 function WebWorker() {
   const [fromWorker, setFromWorker] = React.useState('from server render')
-  const worker = useWebWorker()
-  React.useMemo(
-    () => {
-      worker.postMessage('from first render')
-      worker.onmessage = e => setFromWorker(e.data)
-    },
-    [worker]
-  )
+  const [worker, setWorker] = React.useState(null)
+
+  React.useEffect(() => {
+    const currentWorker = new Worker(require('./test-worker?webworker'))
+    setWorker(currentWorker)
+
+    currentWorker.postMessage('from first render')
+    currentWorker.onmessage = e => setFromWorker(e.data)
+
+    return () => currentWorker.terminate()
+  }, [])
 
   return (
     <>
