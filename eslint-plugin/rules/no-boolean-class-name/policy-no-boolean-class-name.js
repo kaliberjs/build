5cebb03b6808bcfeda3702a25bfb8f3edfa15ec8
larsvankleef <larsvankleef@outlook.com>
2021-06-28 16:12:36 +0200

    WIP extra checks

--- eslint-plugin/rules/no-boolean-class-name/policy-no-boolean-class-name.js
+++ eslint-plugin/rules/no-boolean-class-name/policy-no-boolean-class-name.js
@@ -3,8 +3,12 @@ const { messages } = require('./')
 module.exports = {
   valid: [
     `<div className={cx(fullscreen && styles.component)} />`,
+    `<Card className={cx(fullscreen && styles.component)} />`,
     `<div className={styles.component} />`,
-    `<div className="MenuItem" />`
+    `<div className="MenuItem" />`,
+    `<div id={item && item.id} />`,
+    `<Card id={item && item.id} {...{ item }} key={item.id} />`,
+    `<div className={value ? styles[value] : styles.default} />`
   ],
   invalid: [
     {
