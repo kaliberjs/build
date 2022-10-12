5f97860b8232fd25a715b07d30b839a6125cbd9e
EECOLOR <ewestra@kaliber.net>
2022-10-12 16:18:37 +0200

    Add missing babel transformations

--- library/lib/build.js
+++ library/lib/build.js
@@ -70,11 +70,15 @@ const babelLoader = {
     babelrc: false, // this needs to be false, any other value will cause .babelrc to interfere with these settings
     presets: ['@babel/preset-react'],
     plugins: [
-      '@babel/syntax-dynamic-import',
-      '@babel/syntax-optional-chaining',
-      ['@babel/proposal-decorators', { legacy: true }],
-      '@babel/proposal-class-properties',
+      '@babel/plugin-proposal-class-properties',
+      ['@babel/plugin-proposal-decorators', { legacy: true }],
+      '@babel/plugin-proposal-export-namespace-from',
       '@babel/plugin-proposal-nullish-coalescing-operator',
+      '@babel/plugin-proposal-object-rest-spread',
+      '@babel/plugin-proposal-optional-chaining',
+      '@babel/syntax-dynamic-import',
+      '@babel/plugin-transform-named-capturing-groups-regex',
+      '@babel/plugin-transform-template-literals',
     ]
   }
 }
