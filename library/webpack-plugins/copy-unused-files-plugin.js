eaf170c26b08e599db080e788b95d541d3adf2a3
EECOLOR <ewestra@kaliber.net>
2023-12-28 13:43:50 +0100

    Prevent additional stats call by using a different library function

--- library/webpack-plugins/copy-unused-files-plugin.js
+++ library/webpack-plugins/copy-unused-files-plugin.js
@@ -20,31 +20,29 @@ module.exports = function copyUnusedFilesPlugin() {
         const chunk = {
           name: 'unused file',
           ids: ['_'],
-          files: []
+          files: /** @type {Array<string>} */([])
         }
         compilation.chunks.push(chunk)
 
         const filesCopied = Promise.all(
-          walkSync(context).map(filePath => {
-            const source = path.resolve(context, filePath)
+          walkSync.entries(context).map(entry => {
+            const source = entry.fullPath
 
             if (compilation.fileDependencies.has(source)) return null
 
+            const filePath = entry.relativePath
             const target = path.resolve(compiler.options.output.path, filePath)
-            return stat(source)
-              .then(stats => {
-                if (stats.isDirectory()) return
-                if (compilation.assets[filePath]) throw new Error(`Unexpected problem: ${filePath} is marked as unused but it is in the compilation assets - create an issue with an example to reproduce and we will figure out a solution`)
+            if (entry.isDirectory()) return null
+            if (compilation.assets[filePath]) throw new Error(`Unexpected problem: ${filePath} is marked as unused but it is in the compilation assets - create an issue with an example to reproduce and we will figure out a solution`)
 
-                const asset = compilation.assets[filePath] = {
-                  size: () => stats.size,
-                  emitted: false
-                }
-                chunk.files.push(filePath)
+            const asset = compilation.assets[filePath] = {
+              size: () => entry.size,
+              emitted: false
+            }
+            chunk.files.push(filePath)
 
-                return copy(source, target)
-                  .then(_ => { asset.emitted = true })
-              })
+            return copy(source, target)
+              .then(_ => { asset.emitted = true })
           })
         )
 
@@ -54,14 +52,10 @@ module.exports = function copyUnusedFilesPlugin() {
   }
 }
 
+/** @returns {Promise<void>} */
 function copy(source, target) {
   return new Promise((resolve, reject) => {
     fs.copy(source, target, { preserveTimestamps: true }, err => { err ? reject(err) : resolve() })
   })
-}
 
-function stat(filePath) {
-  return new Promise((resolve, reject) => {
-    fs.stat(filePath, (err, stats) => { err ? reject(err) : resolve(stats) })
-  })
 }
