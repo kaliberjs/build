cb24f10af289194e973c1fd7ee55db265d83a6c1
EECOLOR <ewestra@kaliber.net>
2024-11-01 16:06:03 +0100

    Improve error message

--- library/lib/SafeScript.js
+++ library/lib/SafeScript.js
@@ -6,7 +6,7 @@ let scriptHashes = null
 module.exports = { SafeScript, recordScriptHashes }
 
 function SafeScript({ dangerouslySetInnerHTML }) {
-  if (!scriptHashes) throw new Error('No script hashes present')
+  if (!scriptHashes) throw new Error('No script hashes present, can not use SafeScript in a static environment, check if your *.html.js file returns a function')
   scriptHashes.add(crypto.createHash('sha256').update(dangerouslySetInnerHTML.__html).digest('base64'))
   return React.createElement('script', { dangerouslySetInnerHTML })
 }
