977bbeab32d8cc04ab86a6b73acde9d67379c827
EECOLOR <ewestra@kaliber.net>
2024-11-05 13:18:42 +0100

    Make static templates functional

--- library/lib/SafeScript.js
+++ library/lib/SafeScript.js
@@ -1,19 +1,17 @@
 const crypto = require('crypto')
 const React = require('react')
 
-let scriptHashes = null
-
 module.exports = { SafeScript, recordScriptHashes }
 
 function SafeScript({ dangerouslySetInnerHTML }) {
-  if (!scriptHashes) throw new Error('No script hashes present, can not use SafeScript in a static environment, check if your *.html.js file returns a function')
-  scriptHashes.add(crypto.createHash('sha256').update(dangerouslySetInnerHTML.__html).digest('base64'))
+  if (!global.scriptHashes) throw new Error('No script hashes present')
+  global.scriptHashes.add(crypto.createHash('sha256').update(dangerouslySetInnerHTML.__html).digest('base64'))
   return React.createElement('script', { dangerouslySetInnerHTML })
 }
 
 function recordScriptHashes(newScriptHashses, callback) {
-  scriptHashes = newScriptHashses
+  global.scriptHashes = newScriptHashses
   const result = callback()
-  scriptHashes = null
+  global.scriptHashes = null
   return result
 }
